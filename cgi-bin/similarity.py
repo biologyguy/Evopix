87fb59ba1fc5fd0029ec754b9dbff6b3b7841e4f
Steve Bond <biologyguy@gmail.com>
2014-09-04 09:21:31 -0400

    similarity.py: I didn't actually push all the changes in the previous commit. See previous commit message.

--- cgi-bin/similarity.py
+++ cgi-bin/similarity.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python3
 from Evopic import Evopic
 
-def similarity_score(evo1, evo2):
-    sim_score = 0.
 
-    #Match path IDs
-    evo1_path_ids = []
-    for path in evo1.paths:
-        evo1_path_ids.append(int(path["path_id"][:-1]))
+def similarity_score(evo_1, evo_2):
+    """
+    Weightings: - Path presence/absence (weighted by size)
+                - Path similarity between shared paths
 
-    evo2_path_ids = []
-    for path in evo2.paths:
-        evo2_path_ids.append(int(path["path_id"][:-1]))
+    """
+    sim_score = 1.
 
-    for id in evo1_path_ids:
-        if id in evo2_path_ids:
-            print evo2_path_ids.index(id)
+    #Match path IDs, and subtract the size of non-shared paths
+    evo_1_path_ids = []
+    evo_1_path_sizes = []
+    for path in evo_1.paths:
+        evo_1_path_ids.append(int(path["path_id"][:-1]))
+        evo_1_path_sizes.append(len(path["points"]))
+
+    evo_2_path_ids = []
+    evo_2_path_sizes = []
+    for path in evo_2.paths:
+        evo_2_path_ids.append(int(path["path_id"][:-1]))
+        evo_2_path_sizes.append(len(path["points"]))
+
+    matched_paths = []
+    for path_id in list(evo_1_path_ids):
+        if path_id in evo_2_path_ids:
+            matched_paths.append(path_id)
+            del evo_1_path_ids[evo_1_path_ids.index(path_id)]
+            del evo_2_path_ids[evo_2_path_ids.index(path_id)]
+
+    print(evo_1_path_sizes)
+    print(evo_2_path_sizes)
+    print(matched_paths)
 
     return sim_score
 
@@ -29,3 +46,6 @@ if __name__ == '__main__':
         sue = Evopic(infile.read())
 
     print(similarity_score(bob, sue))
+
+    for i in bob.paths[0]:
+        print("%s: %s" % (i, bob.paths[0][i]))
