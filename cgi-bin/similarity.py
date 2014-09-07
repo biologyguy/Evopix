2f6808af29ed2c047d0e8012baa49b555acb3954
Steve Bond <biologyguy@gmail.com>
2014-09-07 14:12:46 -0400

    similarity.py: The function can now accept two evopix, and calculates a similarity score based on path presence/absence, point presence/absence, and how far appart common points are from one another. Still needs to account for differences in gradients and strokes.

--- cgi-bin/similarity.py
+++ cgi-bin/similarity.py
@@ -24,11 +24,11 @@ def find_path_area(path):  # Input is an individual path from Evopic.paths
 def find_path_length(path):  # Input is an individual path from Evopic.paths
     length = 0.
     ox, oy = path["points"][0]["coords"][1]
-    del path["points"][0]
-    for i in path["points"]:
+    for i in path["points"][1:]:
         length += line_length([ox, oy], i["coords"][1])
         ox, oy = i["coords"][1]
     return length
+
 
 def line_length(point_a, point_b):  # implement Pythagorean theorem
     length = (abs(point_a[0] - point_b[0])**2 + abs(point_a[1] - point_b[1])**2)**0.5
@@ -39,7 +39,7 @@ def match_path_points(path_1, path_2):
     """
     Returns the abs differences between control-point-control points for all matched points in the paths, and counts
     the number of unmatched points
-    :rtype : {"matched": [], "unmatched": int}
+    :rtype : {"matched": [floats], "unmatched": int}
     :param path_1: Evopic.paths[<path_id>] 
     :param path_2: Evopic.paths[<path_id>] matched to path_1
     """
@@ -76,9 +76,7 @@ def similarity_score(evo_1, evo_2):
     Weightings: - Path presence/absence (weighted by size)
                 - Path similarity between shared paths
     """
-    sim_score = 1.
-
-    #Match path IDs, and account for non-shared paths
+    #Match path IDs, and count # points so similarity can be weighted to path sizes
     total_points = 0
     matched_points = 0
     matched_paths = []
@@ -92,39 +90,38 @@ def similarity_score(evo_1, evo_2):
     for path_id in evo_2.paths_z_pos:
         total_points += len(evo_2.paths[path_id]["points"])
 
-    sim_score *= float(matched_points)/float(total_points)
-
     #Run through each matched path, and get the weighted average (based on number of points) sim score.
-    total_point_count = 0
     path_sim_info = {"ids": [], "num_points": [], "sim_scores": []}
     for path_id in matched_paths:
         path_1, path_2 = [evo_1.paths[path_id], evo_2.paths[path_id]]
+
         # For closed paths, the size of the path is based on area
         if evo_1.paths[path_id]["type"] in ["r", "l"]:
             ave_path_area = (find_path_area(path_1) + find_path_area(path_2))/2.
             path_size = ave_path_area ** 0.5  # Used to assess the magnitude of differences between points
 
-            num_points_in_paths = len(path_1["points"]) + len(path_2["points"])
-            total_point_count += num_points_in_paths
-
-            path_sim_info["ids"].append(path_id)
-            path_sim_info["num_points"].append(num_points_in_paths)
-
-            point_matches = match_path_points(path_1, path_2)
-
-            matches_sim_score = sum(point_matches["matches"])/len(point_matches["matches"])
-            matches_sim_score = 1. - (matches_sim_score/(matches_sim_score + path_size))
-
-            final_points_sim_score = matches_sim_score * (1. - (point_matches["unmatched"] / num_points_in_paths))
-            print(final_points_sim_score)
-
         # For open paths, the size is based on total path length
         else:
-            print(find_path_length(path_1))
-            #ave_path_length = (find_path_length(path_1) + find_path_length(path_2))/2.
+            path_size = (find_path_length(path_1) + find_path_length(path_2))/2.
 
+        num_points_in_paths = len(path_1["points"]) + len(path_2["points"])
 
-        #break
+        point_matches = match_path_points(path_1, path_2)
+
+        matches_sim_score = sum(point_matches["matches"])/len(point_matches["matches"])
+        matches_sim_score = 1. - (matches_sim_score/(matches_sim_score + path_size))
+
+        final_points_sim_score = matches_sim_score * (1. - (point_matches["unmatched"] / num_points_in_paths))
+
+        path_sim_info["ids"].append(path_id)
+        path_sim_info["num_points"].append(num_points_in_paths)
+        path_sim_info["sim_scores"].append(final_points_sim_score)
+
+    sim_score = 0.
+    for i in range(len(path_sim_info["ids"])):
+        sim_score += path_sim_info["num_points"][i] / total_points * path_sim_info["sim_scores"][i]
+
+    # Still need to include similarity info on strokes and gradients
     return sim_score
 
 
@@ -136,7 +133,6 @@ if __name__ == '__main__':
     with open("../genomes/bubba.evp", "r") as infile:
         sue = Evopic(infile.read())
 
-    print("\n%s\n" % similarity_score(bob, sue))
+    print("Similarity score\n%s\n" % similarity_score(bob, sue))
 
-    print(bob.paths[1]["points"])
 
