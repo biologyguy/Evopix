2db55be1d4248c29313ac0adc4ab546beb4d56b7
biologyguy <biologyguy@gmail.com>
2014-09-12 23:32:25 -0400

    similarity.py: starts to impliment point mutations

--- cgi-bin/mutation.py
+++ cgi-bin/mutation.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 from Evopic import Evopic
+from similarity import find_path_area, find_path_length, line_length
+from random import random
+from math import log, pi, cos, sin, radians
 
 
-def number_of_mutations(sample_size, probability):  # This is going to return a value from a poisson distribution
-    return "Some number"
+def move_point(path_size, point):
+    distance_changed = 0.05 * path_size  # This needs to take into account the size of the path
+    direction_change = (random() * 360)
+    change_x = sin(radians(direction_change)) * distance_changed
+    change_y = cos(radians(direction_change)) * distance_changed
+    return [change_x, change_y]
 
 
 def mutate(evopic):
+    mutation_rates = {"points": 0.025, "gradient": 0.01}
+
     for path_id in evopic.paths_z_pos:
-        print(path_id)
+        path = evopic.paths[path_id]
+        if path["type"] in ["r", "l"]:
+            size = find_path_area(path) ** 0.5
+        else:
+            size = find_path_length(path)
+
+        for point in evopic.paths[path_id]["points"]:
+            if random() < mutation_rates["points"]:
+                print(move_point(size, point))
+        #break
+
     return evopic
 
 #-------------------------Sandbox-------------------------------#
@@ -16,7 +35,8 @@ if __name__ == '__main__':
     with open("../genomes/bob.evp", "r") as infile:
         bob = Evopic(infile.read())
         bob = mutate(bob)
-        print(bob.svg_out())
+        #print(bob.svg_out())
 
-    with open("../genomes/test.svg", "w") as ofile:
-        ofile.write(bob.svg_out())
+    #with open("../genomes/test.svg", "w") as ofile:
+    #    ofile.write(bob.svg_out())
+
