149fc4f0608087ed0d2ee61e3dcfbdfab536b559
jhawkey <jhawkey@student.unimelb.edu.au>
2014-09-16 11:55:07 +1000

    including paths, attempting to construct new evp file

--- cgi-bin/sexual_reproduction.py
+++ cgi-bin/sexual_reproduction.py
@@ -8,11 +8,11 @@ def parse_args():
 
     parser = ArgumentParser(description='Create paths for a new offspring from mum and dad')
 
-    #parser.add_argument('--mother', type=str, required=False, help='Mother evopic to breed with', default='/Users/jane/Documents/scripts_local/Evopix/genomes/sue.evp')
-    #parser.add_argument('--father', type=str, required=False, help='Father evopic to breed with', default='/Users/jane/Documents/scripts_local/Evopix/genomes/bob.evp')
+    parser.add_argument('--mother', type=str, required=False, help='Mother evopic to breed with', default='/Users/jane/Documents/scripts_local/Evopix/genomes/bubba.evp')
+    parser.add_argument('--father', type=str, required=False, help='Father evopic to breed with', default='/Users/jane/Documents/scripts_local/Evopix/genomes/bob.evp')
 
-    parser.add_argument('--mother', type=str, required=False, help='Mother evopic to breed with', default='/var/www/Evopix/genomes/sue.evp')
-    parser.add_argument('--father', type=str, required=False, help='Father evopic to breed with', default='/var/www/Evopix/genomes/bob.evp')
+    #parser.add_argument('--mother', type=str, required=False, help='Mother evopic to breed with', default='/var/www/Evopix/genomes/sue.evp')
+    #parser.add_argument('--father', type=str, required=False, help='Father evopic to breed with', default='/var/www/Evopix/genomes/bob.evp')
 
     return parser.parse_args()
 
@@ -33,20 +33,77 @@ def main():
     #print(boy.paths[1].points)
 
     #--------Try looping something like this--------#
-    mismatch_paths = []
+    mismatch_paths = {'girl': [], 'boy': []}
+    included_paths = {'girl': [], 'boy': []}
+
+    for path_id in girl.paths_order:
+        if path_id not in boy.paths_order:
+            mismatch_paths['girl'].append(path_id)
     for path_id in boy.paths_order:
         if path_id not in girl.paths_order:
-            mismatch_paths.append(path_id)
+            mismatch_paths['boy'].append(path_id)
             continue
-
+        
         boy_path = boy.paths[path_id]
         girl_path = girl.paths[path_id]
         print("Boy path ID:%s\nPoint IDs\tCoords" % path_id)
         for point_id in boy_path.points_order:
             coords = boy_path.points[point_id]
             print("\t%s\t\t%s" % (point_id, coords))
+        flip = random.randint(0, 1)
+        if flip == 1:
+            included_paths['boy'].append(path_id)
+        else:
+            included_paths['girl'].append(path_id)
 
-        sys.exit("Exit at line 49")
+    for path in mismatch_paths['girl']:
+        flip = random.randint(0, 1)
+        if flip == 1:
+            included_paths['girl'].append(path)
+    for path in mismatch_paths['boy']:
+        flip = random.randint(0, 1)
+        if flip == 1:
+            included_paths['boy'].append(path)
+    print(included_paths)
+
+    new_path_set = []
+    new_path_order = []
+
+    for path in included_paths['boy']:
+        new_path_set.append(boy.paths[path])
+        new_path_order.append(path)
+    for path in included_paths['girl']:
+        new_path_set.append(girl.paths[path])
+        new_path_order.append(path)
+    print(new_path_set)
+    print(new_path_order)
+
+    baby = ''
+    print(new_path_set[2])
+    for path_id in new_path_order:
+        print(path_id)
+        path = new_path_set[path_id]
+        baby += "p%s%s:" % (path.id, path.type)
+        for point_id in path.points_order:
+            coords = path.points[point_id]
+            baby += "t%s~%s,%s;%s,%s;%s,%s;" % (point_id, coords[0][0], coords[0][1], coords[1][0],
+                                                   coords[1][1], coords[2][0], coords[2][1])
+
+        if path.type in ["r", "l"]:  # skip if the path is not closed
+            baby += ":r,%s,%s,%s,%s,%s" % tuple(path.radial)
+            baby += ":l,%s,%s,%s,%s:" % tuple(path.linear)
+
+            for stop in path.stops:
+                params = stop["params"]
+                baby += "o%s~%s,%s,%s;" % (stop["stop_id"], params[0], params[1], params[2])
+
+        baby += ":s%s,%s,%s\n" % tuple(path.stroke)
+    baby_evo = Evopic(baby)
+    print(baby_evo)
+
+
+
+    sys.exit("Exit at line 49")
     #------------------------------------------------#
 
     for path in boy.paths:
@@ -70,25 +127,6 @@ def main():
             baby_points.append(new_coord_set)
         print(new_coord_set)
     print(baby_points)
-
-
-            #print('this is the boys')
-            #print(coord_list)
-            #print('this is the corresponding girls')
-            #print(girl_points[coord_set]['coords'])
-
-
-
-    '''for path_id in boy.paths:
-        if path_id in girl.paths:
-            for coordinate_set in boy.paths[path_id]['points']:
-                print(coordinate_set['coords'])
-                coord_no = 0
-                for coord in coordinate_set['coords']:
-                    #this line doesn't allow me to get to the correct coord for the girl evopic... I'm getting lost
-                    #in my parsing!
-                    new_coord = (coord[0] * prop + girl.paths[path_id]['points'][coordinate_set.index()]['coords'][0] * prop)'''
-                    #print(new_coord) 
                 
 if __name__ == '__main__':
     main()
