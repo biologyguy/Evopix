76ee4057b6a01e11e102676e007bcfc0d29d686b
biologyguy <biologyguy@gmail.com>
2014-09-25 21:56:11 -0400

    Still messing around with Django, but haven't quite figured it out yet.

--- website/urls.py
+++ website/urls.py
@@ -4,7 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       url(r'^$', 'views.index'),
+                       url(r'^/', 'templates.index'),
+                       url(r'^home/', 'templates.welcome'),
                        (r'^farm/', include('farm.urls')),
                        (r'^evopix/', include('evopix.urls')),
                        url(r'^admin/', include(admin.site.urls)),
