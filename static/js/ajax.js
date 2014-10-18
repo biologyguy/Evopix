4110db40356bfd565f76a60ea7926bb787008b62
biologyguy <biologyguy@gmail.com>
2014-10-17 23:02:21 -0400

    New dedicated js file that contains the necessary code to allow AJAX to run. Needs to be included in the head of any template that will be making calls.

--- static/js/ajax.js
+++ static/js/ajax.js
@@ -1,3 +1,19 @@
 /**
  * Created by beako on 10/17/14.
  */
+$(document).ready(function(){
+        var csrftoken = $.cookie('csrftoken');
+
+        function csrfSafeMethod(method) {
+                // these HTTP methods do not require CSRF protection
+                return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+        }
+
+        $.ajaxSetup({
+            beforeSend: function(xhr, settings) {
+                if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            }
+        });
+    });
