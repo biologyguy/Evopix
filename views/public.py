5f544c46765c5a0000b2cadb50d9df8a50bfc5e5
Steve Bond <biologyguy@gmail.com>
2014-10-24 22:57:02 -0400

    Slow but surely unbreaking everything... There is still some really odd behavior that I need to sort out though.

--- views/public.py
+++ views/public.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from evp.models import *
 from resources.Evopic import Evopic
 from resources import mutation, breed
-
+import re
 
 # Create your views here.
 def index(request):
@@ -15,17 +15,17 @@ def index(request):
 
 def not_found(request):
     if request.method == "POST":
-        evp = request.POST.get('evp', '')
+        evp = re.sub("[\[\]]", "", request.POST.get('evp', ''))
         bob = Evopic(evp)
         output = {"svg": bob.svg_out(bounding_box=(325, 325)), "evp": bob.evp}
         for i in range(1, 9):
             baby = mutation.mutate(Evopic(evp))
-            baby = Evopic(baby)
-            output["svg%s" % i] = baby.svg_out(bounding_box=180)
+
+            output["svg%s" % i] = baby.svg_out(bounding_box=(180, 180))
             output["evp%s" % i] = baby.evp
 
         return render(request, 'templates/404.html', output)
 
     else:
         bob = Evopic(Evopix.objects.all()[1].evp)
-        return render(request, 'templates/404.html', {"svg": bob.svg_out(), "evp": bob.evp})
+        return render(request, 'templates/404.html', {"svg": bob.svg_out(bounding_box=(325, 325)), "evp": bob.evp})
