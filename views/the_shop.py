7da1c7bd502ac87764cdebb602a7d819bb34f0e3
biologyguy <biologyguy@gmail.com>
2014-12-18 23:27:20 -0500

    Working on the Shop
    -Changed the border1 image to include a background fill, and updated the css to properly render it
    -Pulled all the database data for the stuff in the shop, and sent it to the template to be looped over and output into tables. It looks really ugly right now, but getting some functionality in place at least.

--- views/the_shop.py
+++ views/the_shop.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 
 from evp.models import *
+from the_shop.models import *
 from resources.Evo import Evopic
 from resources import mutation, breed
 import re
@@ -14,6 +15,10 @@ from django.contrib.auth.decorators import login_required
 def store(request):
     if request.user.is_authenticated():
         user_id = request.user.id
+        breeding_pellets = BreedingPellets.objects.all()
+        fences = Fences.objects.all()
+        grasseed = GrassSeed.objects.all()
+        return render(request, 'templates/store.html', {"user_id": user_id, "breeding_pellets": breeding_pellets,
+                                                        "fences": fences, "grasseed": grasseed})
     else:
         return HttpResponse("Something has gone wrong... Can't find your user name in farm view.")
-    return render(request, 'templates/store.html', {"user_id": user_id})
